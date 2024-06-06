73fd68991d9c454742188bbf989590eb0d3c37db
usedsthe <usedsthe@gmail.com>
2024-06-06 17:56:56 +0000

    .

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Office;
 
 class SearchController extends Controller
 {
     public function searchPage(Request $req){
 
         $type = $req->type;
+        dd(Office::where("type", $type)->update(["search"=> $req->all()]));
         return view('search');
     }
 
